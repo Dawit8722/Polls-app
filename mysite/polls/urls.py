c5bee2f034ca1d8c4f04b60c46928355a8841ead
Dawit8722 <dawit8722@gmail.com>
2021-08-25 12:56:40 +0300

    all

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,6 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('int:question_id/', views.detail, name='detail'),
+    path('int:question_id>/results/', views.detail, name='results')
+    path('int:question_id/vote/', views.detail, name='vote')
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ]
